fa16ab233b859488e3a85e7b415f1216af27e56d
Idan-Fishman <IdanFishmanSWE@gmail.com>
2022-05-28 09:55:58 +0300

    setup react router

---
+++ ui/src/pages/admin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Admin = () => {
+  return (
+    <>
+      <p>Admin Page</p>
+    </>
+  );
+};
+
+export default Admin;
---
+++ ui/src/pages/stats.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <>
+      <p>Stats Page</p>
+    </>
+  );
+};
+
+export default Stats;
